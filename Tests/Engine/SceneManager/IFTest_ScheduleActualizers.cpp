6fef4942390bd112087bbd1269eb308f5cb20800
Nikolay Gorbachenko <gorbachenko@magenta-technology.ru>
2016-03-02 14:50:48 +0400

    #15 Test for duration calculation

--- Tests/Engine/SceneManager/IFTest_ScheduleActualizers.cpp
+++ Tests/Engine/SceneManager/IFTest_ScheduleActualizers.cpp
@@ -20,6 +20,94 @@
 #include <engine/SceneManager/ScheduleActualization/Algorithms/StopDurationActualizationAlgorithm.h>
 #include <Engine/SceneManager/ScheduleActualization/Algorithms/StopArrivalTimeActualizationAlgorithm.h>
 
+TEST_CASE("ScheduleActualizers - StopDurationActualizationAlgorithm", "[integration][schedule_actualizers]")
+{
+    using namespace Scheduler;
+
+    CrowFlyRoutingService routing_service;
+
+    SceneManager sm;
+
+    sm.setRoutingService(&routing_service);
+
+    Location start_location = make_location(0, 0);
+    Location end_location = make_location(0, 0.5);
+
+    Location loc1 = make_location(0, 0.1);
+    Location loc2 = make_location(0, 0.2);
+    Location loc3 = make_location(0, 0.3);
+    Location loc4 = make_location(0, 0.4);
+
+    Scene* s = sm.createScene();
+
+
+    Performer* performer = s->createPerformer();
+    Vehicle* vehicle = s->createVehicle();
+
+    Schedule* schedule = s->createSchedule(performer);
+    schedule->setDepotLocation(start_location);
+    schedule->setShiftEndLocation(end_location);
+
+    schedule->getScheduleActualizer()->createAlgorithm<StopDurationActualizationAlgorithm>();
+
+    Run* r = schedule->createRun(start_location, end_location);
+    r->setVehicle(vehicle);
+
+    SECTION("Simple duration check")
+    {
+
+        Duration dur = Units::minutes(10);
+
+        Operation* sop = s->createFreeOperation();
+        sop->setDuration(dur);
+        sop->setLocation(start_location);
+
+        Operation* sop2 = s->createFreeOperation();
+        sop2->setDuration(dur);
+        sop2->setLocation(start_location);
+
+        Operation* op1 = s->createFreeOperation();
+        op1->setDuration(dur);
+        op1->setLocation(loc1);
+
+        Operation* op2 = s->createFreeOperation();
+        op2->setDuration(dur);
+        op2->setLocation(loc2);
+
+        Operation* op3 = s->createFreeOperation();
+        op3->setDuration(dur);
+        op3->setLocation(loc3);
+
+        Operation* op4 = s->createFreeOperation();
+        op4->setDuration(dur);
+        op4->setLocation(loc4);
+
+        Operation* eop = s->createFreeOperation();
+        eop->setDuration(dur);
+        eop->setLocation(end_location);
+
+        r->allocateStartOperation(sop);
+        r->allocateStartOperation(sop2);
+        r->allocateEndOperation(eop);
+
+        Stop *s1 = r->getStartStop();
+        Stop *s2 = r->allocateWorkOperation(op1, 0);
+        Stop *s3 = r->allocateWorkOperation(op2, 1);
+        Stop *s4 = r->allocateWorkOperation(op3, 2);
+        Stop *s5 = r->allocateWorkOperation(op4, 3);
+        Stop *s6 = r->getEndStop();
+
+        REQUIRE(s1->getDuration() == dur*2);
+        REQUIRE(s2->getDuration() == dur);
+        REQUIRE(s3->getDuration() == dur);
+        REQUIRE(s4->getDuration() == dur);
+        REQUIRE(s5->getDuration() == dur);
+        REQUIRE(s6->getDuration() == dur);
+    }
+
+    sm.destroyScene(s);
+}
+
 TEST_CASE("ScheduleActualizers - StopArrivalTimeActualizationAlgorithm", "[integration][schedule_actualizers]")
 {
     using namespace Scheduler;
@@ -108,7 +196,7 @@ TEST_CASE("ScheduleActualizers - StopArrivalTimeActualizationAlgorithm", "[integ
 
         TimeWindow estimated_allocation_6;
         estimated_allocation_6.setStartTime(estimated_allocation_5.getEndTime() + r5.getDuration());
-        estimated_allocation_6.setEndTime(estimated_allocation_6.getStartTime() + dur);
+        estimated_allocation_6.setEndTime(estimated_allocation_6.getStartTime());
 
         REQUIRE(s1->getAllocationTime() == estimated_allocation_1);
         REQUIRE(s2->getAllocationTime() == estimated_allocation_2);
