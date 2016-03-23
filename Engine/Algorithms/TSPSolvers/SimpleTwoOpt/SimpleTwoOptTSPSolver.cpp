d59b5deb66751dc71329d414e9023a699ee24836
Alexander Zhukov <outsideroff@gmail.com>
2016-03-23 23:09:10 +0400

    [#22] Implemented initial version of SimpleTwoOptTSPSolver

--- Engine/Algorithms/TSPSolvers/SimpleTwoOpt/SimpleTwoOptTSPSolver.cpp
+++ Engine/Algorithms/TSPSolvers/SimpleTwoOpt/SimpleTwoOptTSPSolver.cpp
@@ -1,4 +1,8 @@
 #include "SimpleTwoOptTSPSolver.h"
+#include <Engine/SceneManager/Schedule.h>
+#include <Engine/SceneEditor/Actions/SwapRunWorkStops.h>
+#include <Engine/SceneEditor/Actions/ReverseRunWorkStopsSubsequence.h>
+#include <Engine/SceneManager/Run.h>
 
 namespace Scheduler
 {
@@ -7,14 +11,41 @@ namespace Scheduler
 	{
 	}
 
-	void SimpleTwoOptTSPSolver::optimize(Schedule* schedule) const
+    void SimpleTwoOptTSPSolver::optimize(Schedule* schedule) const
 	{
 		if (!schedule_cost_function) return; // We don't have a metric to optimize - so we can't
+
+        const auto &runs = schedule->getRuns();
+        for (auto run_it = runs.begin(); run_it != runs.end(); ++run_it) {
+            optimize(schedule, std::distance(run_it, runs.end() - 1));
+        }
 	}
 
 	void SimpleTwoOptTSPSolver::optimize(Schedule* schedule, size_t run_index) const
 	{
 		if (!schedule_cost_function) return; // We don't have a metric to optimize - so we can't
+
+        const auto &runs = schedule->getRuns();
+        const auto run_ptr = runs.at(run_index);
+        const auto &stops = run_ptr->getWorkStops();
+        Cost best_cost = schedule_cost_function->calculateCost(run_ptr->getSchedule());
+        for (auto stop_it1 = stops.begin(); stop_it1 != stops.end() - 1; ++stop_it1) {
+            for (auto stop_it2 = stop_it1 + 1; stop_it2 != stops.end(); ++stop_it2) {
+                SwapRunWorkStops swapAction(run_ptr, *stop_it1, *stop_it2);
+                ReverseWorkStopsSubsequence reverseAction(run_ptr, *stop_it1, *stop_it2);
+
+                swapAction.perform();
+                reverseAction.perform();
+                Cost cost = schedule_cost_function->calculateCost(schedule);
+                if (cost > best_cost) {
+                    best_cost = cost;
+                } else {
+                    reverseAction.rollback();
+                    swapAction.rollback();
+                }
+
+            }
+        }
 	}
 
 	void SimpleTwoOptTSPSolver::setScheduleCostFunction(ScheduleCostFunction* cost_function)
