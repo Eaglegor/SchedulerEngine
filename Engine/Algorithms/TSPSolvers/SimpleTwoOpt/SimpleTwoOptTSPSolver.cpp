4f361b146889d61e124d3f51f2f5149bc7738c19
Alexander Zhukov <outsideroff@gmail.com>
2016-03-25 21:47:01 +0400

    fix reverse in 2opt optimize

--- Engine/Algorithms/TSPSolvers/SimpleTwoOpt/SimpleTwoOptTSPSolver.cpp
+++ Engine/Algorithms/TSPSolvers/SimpleTwoOpt/SimpleTwoOptTSPSolver.cpp
@@ -37,7 +37,7 @@ namespace Scheduler
                 for (auto stop_it2 = stop_it1 + 1; stop_it2 != stops.end(); ++stop_it2) {
                     SceneEditor editor;
                     editor.performAction<SwapRunWorkStops>(run_ptr, *stop_it1, *stop_it2);
-                    editor.performAction<ReverseWorkStopsSubsequence>(run_ptr, *stop_it1, *stop_it2);
+                    editor.performAction<ReverseWorkStopsSubsequence>(run_ptr, *(stop_it1 + 1), *(stop_it2 - 1));
                     Cost cost = schedule_cost_function->calculateCost(schedule);
                     if (cost < best_cost) {
                         best_cost = cost;
