9e8c063809e95f5ef9d182f9b4eb3d8040547773
eaglegor <eaglegor@gmail.com>
2016-04-03 18:59:28 +0400

    [#65] Added export specifier to ScheduleStateUtils

--- Engine/SceneManager/ScheduleStateUtils.h
+++ Engine/SceneManager/ScheduleStateUtils.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <SceneManager_export.h>
+
 namespace Scheduler
 {
 	class Schedule;
 
-	class ScheduleStateUtils
+	class SCENEMANAGER_EXPORT ScheduleStateUtils
 	{
 	public:
 		static void copyState(const Schedule* from, Schedule* to);
