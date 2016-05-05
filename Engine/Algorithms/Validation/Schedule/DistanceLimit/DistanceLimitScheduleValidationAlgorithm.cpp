72dea89d58e6860db5f141a1794aeb3e5b25e519
eaglegor <eaglegor@gmail.com>
2016-05-06 00:22:12 +0400

    [#50] Added schedule distance limit validator

---
+++ Engine/Algorithms/Validation/Schedule/DistanceLimit/DistanceLimitScheduleValidationAlgorithm.cpp
@@ -0,0 +1,18 @@
+#include "DistanceLimitScheduleValidationAlgorithm.h"
+
+#include <Engine/SceneManager/Schedule.h>
+#include <Engine/SceneManager/Run.h>
+#include <Engine/SceneManager/Vehicle.h>
+#include <Engine/SceneManager/Operation.h>
+#include <Engine/Utils/Aggregators/DistanceAccumulator.h>
+
+Scheduler::DistanceLimitScheduleValidationAlgorithm::DistanceLimitScheduleValidationAlgorithm(Distance limit):
+	limit(limit)
+{
+}
+
+bool Scheduler::DistanceLimitScheduleValidationAlgorithm::isValid(const Schedule * schedule) const
+{
+	if (schedule->getRuns().empty()) return true;
+	return DistanceAccumulator::accumulateDistance(schedule->getRuns().front()->getStartStop(), schedule->getRuns().back()->getEndStop()) > limit;
+}
