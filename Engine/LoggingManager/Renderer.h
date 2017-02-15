cd74cea7efd218cad975ee8bce352857f5ee12f6
Nikolay Gorbachenko <gorbachenko@magenta-technology.ru>
2017-02-15 20:20:18 +0400

    Added debug renderer interface

---
+++ Engine/LoggingManager/Renderer.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <Engine/Utils/String.h>
+
+namespace Scheduler
+{
+	class Run;
+	class Schedule;
+	class Scene;
+	class Site;
+
+	class Frame
+	{
+		virtual ~Frame() {}
+
+		virtual void addRun(const Run& run) = 0;
+		virtual void addSchedule(const Schedule& schedule) = 0;
+		virtual void addScene(const Scene& scene) = 0;
+		virtual void addText(const String& text) = 0;
+		virtual void addTitle(const String& text) = 0;
+		virtual void addLine(const Site& from, const Site& to) = 0;
+		virtual void addPoint(const Site& pt) = 0;
+
+		virtual void draw() = 0;
+	};
+
+	class Renderer
+	{
+	public:
+		virtual ~Renderer() {}
+		
+		virtual Frame& nextFrame() = 0;
+	};
+}
