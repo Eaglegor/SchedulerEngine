e4d372946efce1e109b6f5344cab38003af44597
Eaglegor <eaglegor@gmail.com>
2017-08-20 20:55:07 +0400

    [#44] Added API docs to the RoutingProfile concept

--- Engine/Concepts/RoutingProfile.h
+++ Engine/Concepts/RoutingProfile.h
@@ -6,19 +6,54 @@
 
 namespace Scheduler
 {
-	/// Vehicle routing settings (vehicle type, average speed etc.) - used by routing service to build correct routes
+	/** 
+	 * @brief Represents the rules used by the routing services during the routes calculation
+	 */
 	class CONCEPTS_EXPORT RoutingProfile
 	{
 	public:
+		/** 
+		 * @brief Default constructor
+		 */
 		RoutingProfile( );
+
+		/** 
+		 * @brief Copy constructor
+		 * @param rhs Source routing profile
+		 */
 		RoutingProfile(const RoutingProfile& rhs);
 
+		/** 
+		 * @brief Gets average speed of the vehicle
+		 * @return Average vehicle speed
+		 */
 		const Speed& getAverageSpeed( ) const;
+
+		/** 
+		 * @brief Sets average speed of the vehicle
+		 * @param average_speed New average vehicle speed
+		 */
 		void setAverageSpeed(const Speed& average_speed);
 
+		/**
+		 * @brief Equality operator
+		 * @param rhs Another routing profile
+		 * @return true if this and rhs have the same average speed
+		 */
 		bool operator==(const RoutingProfile& rhs) const;
+
+		/**
+		 * @brief Inequality operator
+		 * @param rhs Another routing profile
+		 * @return true if !(*this == rhs)
+		 */
 		bool operator!=(const RoutingProfile& rhs) const;
 
+		/** 
+		 * @brief Assignment operator
+		 * @param rhs Another routing profile
+		 * @return Reference to this
+		 */
 		RoutingProfile& operator=(const RoutingProfile& rhs);
 
 	private:
