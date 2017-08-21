0f8c59c52f081da5f25b6e0f8355f14ea71638ec
Eaglegor <eaglegor@gmail.com>
2017-08-21 08:54:37 +0400

    [#48] Added API docs for RoutingService interface

--- Engine/Routing/RoutingService.h
+++ Engine/Routing/RoutingService.h
@@ -7,12 +7,29 @@ namespace Scheduler
 	class Site;
 	class RoutingProfile;
 
+	/**	
+	 * @brief Represents the abstract routing service interface
+	 * 
+	 * @details {Routing service takes 2 geolocations (route start and end points) and the routing profile of vehicle
+	 * and returns the route information (distance and driving time). It abstracts the distance matrix concept used
+	 * in the logistics optimization problems.}
+	 */
 	class RoutingService
 	{
 	public:
 		virtual ~RoutingService( )
 		{
 		}
+
+		/** 
+		 * @brief Calculates route information
+		 * 
+		 * @param from Start point of the resulting route
+		 * @param to End point of the resulting route
+		 * @routing_profile Vehicle routing profile to calculate the route for
+		 * 
+		 * @return Route information (driving time and distance)
+		 */
 		virtual Route calculateRoute(const Site& from, const Site& to, const RoutingProfile& routing_profile) const = 0;
 	};
 }
