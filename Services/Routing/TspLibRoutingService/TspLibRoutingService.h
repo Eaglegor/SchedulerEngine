0e96859f4e188230243da709511a1b8de81ea87d
Eaglegor <eaglegor@gmail.com>
2017-08-21 09:30:59 +0400

    [#48] Added API docs for the TspLibRoutingService

--- Services/Routing/TspLibRoutingService/TspLibRoutingService.h
+++ Services/Routing/TspLibRoutingService/TspLibRoutingService.h
@@ -8,16 +8,53 @@
 
 namespace Scheduler
 {
+	/** 
+	 * @brief Routing service providing routes from the custom distance matrix
+	 * 
+	 * @details {This routing service is intended to be used in the TSPLIB benchmarks encoded in XML format 
+	 * (https://www.iwr.uni-heidelberg.de/groups/comopt/software/TSPLIB95/XML-TSPLIB/instances).
+	 * There are no real coordinates of the orders but only a distance matrix provided so for the 
+	 * from and end points only latitude coordinate is used and it's encoding the order index (not real latitude).
+	 * 
+	 * This distance matrix is filled by the TspLibSceneLoader}
+	 */
 	class TSPLIBROUTINGSERVICE_EXPORT TspLibRoutingService : public RoutingService
 	{
 	public:
+		/** 
+		 * @brief Default constructor
+		 */
 		TspLibRoutingService( );
 		~TspLibRoutingService( );
 
+		/** 
+		 * @brief Initializes the distance matrix memory storage
+		 * 
+		 * @param nodes_count Rows/Columns count in the matrix (count of orders in the problem instance)
+		 */
 		void init(std::size_t nodes_count);
 
+		/** 
+		 * @brief Returns a route information from the stored distance matrix
+		 * 
+		 * @details {Route distance is taken from the distance matrix. Route duration is calculated
+		 * as distance / average speed taken from the vehicle routing profile}
+		 * 
+		 * @param from Start point of the route (latitude is used as index, longitude is ignored)
+		 * @param from End point of the route (latitude is used as index, longitude is ignored)
+		 * @param routing_profile Vehicle routing profile
+		 * 
+		 * @return Route information
+		 */
 		virtual Route calculateRoute(const Site& from, const Site& to, const RoutingProfile& routing_profile) const override;
 
+		/** 
+		 * @brief Inserts the new distance value into the distance matrix
+		 * 
+		 * @param ia Index of route start point
+		 * @param ib Index of route end point
+		 * @param distance Distance to store in the matrix
+		 */
 		void insertRoute(std::size_t ia, std::size_t ib, float distance);
 
 	private:
