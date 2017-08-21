2aec1327d88187cd47d85c4f43681327468d0d72
Eaglegor <eaglegor@gmail.com>
2017-08-21 09:17:17 +0400

    [#48] Added API docs for TaillardRoutingService

--- Services/Routing/TaillardRoutingService/TaillardRoutingService.h
+++ Services/Routing/TaillardRoutingService/TaillardRoutingService.h
@@ -6,10 +6,26 @@
 
 namespace Scheduler
 {
-
+	/** 
+	 * @brief Routing service providing the routes calculated as described in the Taillard CVRP benchmark
+	 * 
+	 * @details {Euclidean distance is used in Taillard CVRP benchmark datasets}
+	 */
 	class TAILLARDROUTINGSERVICE_EXPORT TaillardRoutingService : public RoutingService
 	{
 	public:
+		/** 
+		 * @brief Calculates route information using the euclidean distance metric
+		 * 
+		 * @details {Route distance is calculated using euclidean distance metric. Route duration is calculated
+		 * as distance / average speed taken from the vehicle routing profile}
+		 * 
+		 * @param from Start point of the route
+		 * @param to End point of the route
+		 * @param routing_profile Vehicle routing profile
+		 * 
+		 * @return Route information
+		 */
 		virtual Route calculateRoute(const Site& from, const Site& to,
 		                             const RoutingProfile& routing_profile) const override;
 	};
