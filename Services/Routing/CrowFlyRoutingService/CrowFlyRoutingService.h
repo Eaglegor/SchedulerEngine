23e1a8e83abf1efe090a29ee3b3fbb0679a411e2
Eaglegor <eaglegor@gmail.com>
2017-08-21 09:08:51 +0400

    [#48] Added API docs for CrowflyRoutingService

--- Services/Routing/CrowFlyRoutingService/CrowFlyRoutingService.h
+++ Services/Routing/CrowFlyRoutingService/CrowFlyRoutingService.h
@@ -6,10 +6,28 @@
 
 namespace Scheduler
 {
-
+	/** 
+	 * @brief Routing service providing the routes based on "as the crow flies" distance metric
+	 * 
+	 * @details {"As the crow flies or in a beeline is an idiom for the shortest path between two points 
+	 * (on a map, disregarding the vagaries of intervening terrain); the geodesic distance." [Wikipedia]}
+	 */
 	class CROWFLYROUTINGSERVICE_EXPORT CrowFlyRoutingService : public RoutingService
 	{
 	public:
+
+		/** 
+		 * @brief Calculates route information between from and to with respect to the provided routing profile
+		 * 
+		 * @details {Route distance is calculated using crowfly distance metric. Route duration is calculated
+		 * as distance / average speed taken from the vehicle routing profile}
+		 * 
+		 * @param from Start point of route
+		 * @param to End point of route
+		 * @param routing_profile Routing profile of the vehicle
+		 * 
+		 * @return Route information
+		 */
 		virtual Route calculateRoute(const Site& from, const Site& to,
 		                             const RoutingProfile& routing_profile) const override;
 	};
