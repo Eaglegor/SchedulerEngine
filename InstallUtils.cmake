5ba2533aea50e4a8724f0abd76a2765369f1aefb
Eaglegor <eaglegor@gmail.com>
2017-09-04 23:04:02 +0400

    Improvements into install scripts

--- InstallUtils.cmake
+++ InstallUtils.cmake
@@ -1,9 +1,8 @@
-macro(InstallHeaders HEADER_LIST)
-    foreach(header ${HEADER_LIST})
-        message(STATUS ${header})
-        string(REGEX MATCH "(.\\\*)\\\[/\\\]" DIR ${header})
-        file(RELATIVE_PATH REL_DIR ${CPP_SCHEDULER_SOURCE_DIR} ${CMAKE_CURRENT_SOURCE_DIR})
-        install(FILES ${header} DESTINATION include/${REL_DIR}/${DIR})
+macro(InstallHeaders)
+    foreach(header ${ARGN})
+		get_filename_component(DIR "${CMAKE_CURRENT_SOURCE_DIR}/${header}" DIRECTORY)
+        file(RELATIVE_PATH REL_DIR ${CPP_SCHEDULER_SOURCE_DIR} ${DIR})
+        install(FILES ${header} DESTINATION include/${REL_DIR})
     endforeach()
 endmacro()
 
@@ -28,6 +27,11 @@ macro(InstallAlgorithm TARGET_NAME)
             LIBRARY DESTINATION ${CMAKE_BUILD_TYPE}/tools
             RUNTIME DESTINATION ${CMAKE_BUILD_TYPE}/tools
     )
+	
+	install(FILES $<TARGET_PDB_FILE:${TARGET_NAME}> DESTINATION ${CMAKE_BUILD_TYPE}/lib OPTIONAL)
+	install(FILES $<TARGET_PDB_FILE:${TARGET_NAME}> DESTINATION ${CMAKE_BUILD_TYPE}/test OPTIONAL)
+	install(FILES $<TARGET_PDB_FILE:${TARGET_NAME}> DESTINATION ${CMAKE_BUILD_TYPE}/benchmark OPTIONAL)
+	install(FILES $<TARGET_PDB_FILE:${TARGET_NAME}> DESTINATION ${CMAKE_BUILD_TYPE}/tools OPTIONAL)
 endmacro()
 
 macro(InstallCoreComponent TARGET_NAME)
@@ -54,16 +58,22 @@ macro(InstallBenchmark TARGET_NAME)
     install(TARGETS ${TARGET_NAME}
         RUNTIME DESTINATION ${CMAKE_BUILD_TYPE}/benchmark
     )
+	
+	install(FILES $<TARGET_PDB_FILE:${TARGET_NAME}> DESTINATION ${CMAKE_BUILD_TYPE}/benchmark OPTIONAL)
 endmacro()
 
 macro(InstallTool TARGET_NAME)
     install(TARGETS ${TARGET_NAME}
         RUNTIME DESTINATION ${CMAKE_BUILD_TYPE}/tools
     )
+	
+	install(FILES $<TARGET_PDB_FILE:${TARGET_NAME}> DESTINATION ${CMAKE_BUILD_TYPE}/tools OPTIONAL)
 endmacro()
 
 macro(InstallTest TARGET_NAME)
     install(TARGETS ${TARGET_NAME}
         RUNTIME DESTINATION ${CMAKE_BUILD_TYPE}/test
     )
+	
+	install(FILES $<TARGET_PDB_FILE:${TARGET_NAME}> DESTINATION ${CMAKE_BUILD_TYPE}/test OPTIONAL)
 endmacro()
