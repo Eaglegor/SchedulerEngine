d4f7781aac0273f66b1fb85f657f1b03e456631e
Eaglegor <eaglegor@gmail.com>
2017-09-16 17:08:54 +0400

    [#57] Another chunk of API docs

--- Engine/SceneManager/WorkOperation.h
+++ Engine/SceneManager/WorkOperation.h
@@ -2,15 +2,30 @@
 
 #include "Operation.h"
 
-
-
 namespace Scheduler
 {
+	/**
+	 * @ingroup scene_manager
+	 * 
+	 * @brief Operation performed at customer's location
+	 */
 	class WorkOperation : public Operation
 	{
 	public:
+		/**
+		 * @brief Constructor
+		 * 
+		 * @param id Identifier of operation
+		 * @param location Location where the operation has to be performed
+		 * @param order Order the operation belongs to
+		 */
 		WorkOperation(std::size_t id, const Location& location, const Order& order);
 
+		/**
+		 * @brief Returns location where the operation has to be performed
+		 * 
+		 * @return Location where the operation has to be performed
+		 */
 		const Location& getLocation( ) const;
 
 	private:
--- Engine/SceneManager/WorkStop.h
+++ Engine/SceneManager/WorkStop.h
@@ -10,22 +10,72 @@ namespace Scheduler
 {
 	class WorkOperation;
 
+	/**
+	 * @ingroup scene_manager
+	 * 
+	 * @brief Represents work at customer
+	 */
 	class WorkStop : public Stop
 	{
 	public:
+		/**
+		 * @brief Constructor
+		 * 
+		 * @param id Identidier of the stop
+		 * @param context Internal stop context
+		 * @param run Run the stop belongs to
+		 * @param operation Operation that has to be performed
+		 */
 		WorkStop(std::size_t id, const Stop::Context& context, Run& run, const WorkOperation& operation);
 		WorkStop(const WorkStop& rhs) = delete;
 
+		/**
+		 * @brief Returns the identifier of the stop
+		 * 
+		 * @return Identifier of the stop
+		 */
 		std::size_t getId( );
 
+		/**
+		 * @brief Returns the operation that has to be performed
+		 * 
+		 * @return Operation that has to be performed
+		 */
 		const WorkOperation& getOperation( ) const;
 
+		/**
+		 * @brief Returns the location of the stop
+		 * 
+		 * @return Location of the stop
+		 */
 		virtual const Location& getLocation( ) const override;
 
+		/**
+		 * @brief Accepts the stop visitor
+		 * 
+		 * @param Visitor to accept
+		 */
 		void acceptVisitor(StopVisitor& visitor) override;
+
+		/**
+		 * @brief Accepts the stop visitor
+		 * 
+		 * @param Visitor to accept
+		 */
 		void acceptVisitor(ConstStopVisitor& visitor) const override;
 
+		/**
+		 * @brief Equality operator
+		 * 
+		 * @return @c true, if @rhs is the same object as @c this, @c false otherwise
+		 */
 		bool operator==(const WorkStop& rhs) const;
+
+		/**
+		 * @brief Inequality operator
+		 * 
+		 * @return Result of !(rhs == *this)
+		 */
 		bool operator!=(const WorkStop& rhs) const;
 
 	private:
